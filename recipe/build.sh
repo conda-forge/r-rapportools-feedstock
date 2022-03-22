078e54e656f87fee56c3506c0f0cd856de1665a1
Christian Brueffer <christian@brueffer.io>
2022-03-22 09:30:14 +0100

    Partially revert.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-export DISABLE_AUTOBREW=1
-${R} CMD INSTALL --build . ${R_ARGS}
+
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  ${R} CMD INSTALL --build . ${R_ARGS}
+else
+  mkdir -p $PREFIX/lib/R/library/rapportools
+  mv * $PREFIX/lib/R/library/rapportools
+fi
